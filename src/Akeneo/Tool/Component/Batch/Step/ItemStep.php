a7792685c3e0081324ac811c76e6581613be7d6e
Valentin Dijkstra <valentindijkstra@gmail.com>
2023-07-11 10:26:22 +0200

    DO NOT MERGE

--- src/Akeneo/Tool/Component/Batch/Step/ItemStep.php
+++ src/Akeneo/Tool/Component/Batch/Step/ItemStep.php
@@ -85,6 +85,7 @@ class ItemStep extends AbstractStep implements TrackableStepInterface, LoggerAwa
     {
         $itemsToWrite = [];
         $batchCount = 0;
+        $this->batchSize = 10;
 
         $this->initializeStepElements($stepExecution);
 
@@ -93,7 +94,7 @@ class ItemStep extends AbstractStep implements TrackableStepInterface, LoggerAwa
         }
 
         while (true) {
-            sleep(1);
+            sleep(2);
             try {
                 $readItem = $this->reader->read();
                 if (null === $readItem) {
